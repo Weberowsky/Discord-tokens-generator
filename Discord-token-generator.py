917c36f5bcd43b1a890337ba249eb9f698c68242
Weberowsky <36982628+Weberowsky@users.noreply.github.com>
2020-07-24 18:11:41 +0200

    Upload files

---
+++ Discord-token-generator.py
@@ -0,0 +1,40 @@
+import random
+import os
+
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+
+ilosc = input("Ilosc tokenow: \n")
+ilosc = int(ilosc)
+
+os.system('cls')
+
+for i in range(ilosc):
+
+    token1 = ""
+    token2 = ""
+    token3 = ""
+
+    for c in range(24):
+        token1 += random.choice(chars)
+
+    for c in range(6):
+        token2 += random.choice(chars)
+    for c in range(27):
+        token3 += random.choice(chars)
+
+    token1 = str(token1)
+    token2 = str(token2)
+    token3 = str(token3)
+
+    token1 = token1 + "."
+    token2 = token2 + "."
+
+
+    token = token1 + token2 + token3
+
+    print(token)
+
+print("Tokens generated, if you want to copy tokens press [CTRL A] + [CTRL C]")
+input("")
